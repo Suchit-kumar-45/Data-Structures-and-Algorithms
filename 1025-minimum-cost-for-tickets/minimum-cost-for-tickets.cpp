877d1c3e617aecd0b65652e93f492e8a17312a11
Suchit Kumar <suchit844118@gmail.com>
2025-11-04 10:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (18.48%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int>& days, vector<int>& costs,int index,vector<int>&dp)
+    {
+        int n=days.size();
+        //base case
+        if(index>=n)
+        {
+            return 0;
+        }
+        if(dp[index]!=-1)
+            return dp[index];
+        //for days
+        int op1=costs[0]+solve(days,costs,index+1,dp);
+        //for week
+        int i;
+        for( i=0; i<n && days[i]<days[index]+7;i++);
+        int op2=costs[1]+solve(days,costs,i,dp);
+        //for month
+        for(i=0;i<n && days[i]<days[index]+30;i++);
+        int op3=costs[2]+solve(days,costs,i,dp);
+        dp[index]=min({op1,op2,op3});
+        return dp[index];
+
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n=days.size();
+        vector<int>dp(n+1,-1);
+        return solve(days,costs,0,dp);
+    }
+};
