cf9bcf9a9cfbdf7dff4006ee574f969618077d94
Suchit Kumar <suchit844118@gmail.com>
2025-11-10 12:25:21 +0530

    Time: 19 ms (31.24%) | Memory: 25 MB (15.00%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-//using memoization
-    int solve(vector<int>&sat,int index,int time,vector<vector<int>>&dp)
+//using tabulation
+    int solve(vector<int>&sat)
     {
-        //base case
-        if(index==sat.size())
-            return 0;
-        if(dp[index][time]!=-1)
-            return dp[index][time];
-        int incl=sat[index]*(time+1)+solve(sat,index+1,time+1,dp);
-        int excl=0+solve(sat,index+1,time,dp);
-        return dp[index][time]=max(incl,excl);
+        int n=sat.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int time=n-1;time>=0;time--)
+            {
+                int incl=sat[index]*(time+1)+dp[index+1][time+1];
+                int excl=0+dp[index+1][time];
+                dp[index][time]=max(incl,excl);
+            }
+        }
+        
+        return dp[0][0];
     }
     int maxSatisfaction(vector<int>& satisfaction) {
-        int n=satisfaction.size();
+        
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(satisfaction,0,0,dp);   
+        
+        return solve(satisfaction);   
     }
 };
