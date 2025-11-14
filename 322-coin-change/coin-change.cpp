389c1baa69b1c2984a71c2b943a1062d39a4afdb
Suchit Kumar <suchit844118@gmail.com>
2025-11-14 12:03:23 +0530

    Time: 27 ms (61.08%) | Memory: 17.8 MB (82.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int solveMem(vector<int>& coins,int amount,vector<int>&dp)
+    int solveMem(vector<int>& coins,int amount)
     {
-        if(amount==0)
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        
+        for(int i=1;i<=amount;i++)
         {
-            return 0;
-        }
-        if(amount<0)
+        for(int j=0;j<coins.size();j++)
         {
-            return INT_MAX;
+            if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX)
+                 dp[i]=min(dp[i],1+dp[i-coins[j]]);
+            
         }
-        if(dp[amount]!=-1)
-        {
-            return dp[amount];
         }
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {
-            int ans=solveMem(coins,amount-coins[i],dp);
-            if(ans!=INT_MAX)
-            {
-                mini=min(mini,ans+1);
-            }
-        }
-        dp[amount]=mini;
+        if(dp[amount]==INT_MAX)
+            return  -1;
         return dp[amount];
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=solveMem(coins,amount,dp);
-        if(ans==INT_MAX)
-            return -1;
-        return ans;
+        
+        return solveMem(coins,amount);
+        
 
         
     }
