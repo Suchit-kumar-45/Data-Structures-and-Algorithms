fe55814107e4c1ad82dc2a53b420e9a2749fb7e5
Suchit Kumar <suchit844118@gmail.com>
2026-02-20 15:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (43.02%) - LeetSync

--- 390-elimination-game/elimination-game.cpp
+++ 390-elimination-game/elimination-game.cpp
@@ -5,37 +5,7 @@ public:
         {
             return 1;
         }
-        int head=1;
-        int gap=1;
-        int rest=n;
-        bool direction=true;
-        while(rest>1)
-        {
-            if(direction)
-            {
-                head+=gap;
-                gap=gap*2;
-                rest=rest/2;
-                
-            }
-            else
-            {
-                if(rest%2==0)
-                {
-                    gap=gap*2;
-                    rest=rest/2;
-                }
-                else
-                {
-                    head=head+gap;
-                    gap=gap*2;
-                    rest=rest/2;
-                }
-            }
-            direction=!direction;
-
-        }
-        return head;
+        return 2*(n/2+1-lastRemaining(n/2));
         
     }
 };
