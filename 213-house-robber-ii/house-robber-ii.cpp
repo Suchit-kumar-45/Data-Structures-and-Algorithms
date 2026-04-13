7e7c789aae6849fc6ee9a1b2ffaf75d692a40cf9
Suchit Kumar <suchit844118@gmail.com>
2026-04-13 20:33:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (50.64%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,int start,vector<int>&dp)
+    int solve(vector<int>&nums,int s,int e,vector<int>&dp)
     {
-        if(n==start)
-            return nums[start];
-        if(n<start)
+        if(s>e)
             return 0;
-        if(dp[n]!=-1)
-            return dp[n];
-        
-        int incl=solve(nums,n-2,start,dp)+nums[n];
-        int excl=solve(nums,n-1,start,dp)+0;
-        dp[n]=max(incl,excl);
-        return dp[n];
+
+        if(s==e)
+            return nums[e];
+        if(dp[s]!=-1)
+            return dp[s];
+        int incl=nums[s]+solve(nums,s+2,e,dp);
+        int excl=solve(nums,s+1,e,dp);
+        return dp[s]=max(incl,excl);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)
             return nums[0];
-        vector<int>dp1(n,-1);
-        int ans1=solve(nums,n-1,1,dp1);
-
-        vector<int>dp2(n,-1);
-        int ans2=solve(nums,n-2,0,dp2);
-        
+        vector<int>dp1(n+1,-1);
+        vector<int>dp2(n+1,-1);
+        int ans1=solve(nums,0,n-2,dp1);
+        int ans2=solve(nums,1,n-1,dp2);
         return max(ans1,ans2);
+        
     }
 };
